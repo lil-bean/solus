7829e36641489ac503b4bda9c0a57fe523978579
aileen <al7168@bard.edu>
2017-03-12 15:42:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -307,7 +307,7 @@ function draw() {
         fill(161, 177, 204, 100);
         rect(300, 300, 200, 50);
         fill(255, 180);
-        text("P L A Y  A G A I N", 350, 330);
+        text("K E E P  P L A Y I N G", 350, 330);
         if (mouseIsPressed && mouseX > 300 && mouseX < 500 && mouseY > 300 && mouseY < 350) {
             level = 0
         }
